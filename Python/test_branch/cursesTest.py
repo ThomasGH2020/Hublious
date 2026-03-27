acc17273c736363ddf03d42d3ebdd81318e141b4
Thomas Allan <s4883531@student.uq.edu.au>
2026-03-27 18:33:56 +1000

    Test python program showing basic curses implementation.

--- Python/test_branch/cursesTest.py
+++ Python/test_branch/cursesTest.py
@@ -1,3 +1,38 @@
 import curses
+import time
 
-curses.initscr()
+scr = curses.initscr()
+
+# disable the displaying of stdin to terminal
+curses.noecho()
+
+# non-blocking mode
+#curses.cbreak()
+
+# terminal beep sound
+curses.beep()
+
+# flash the screen white for a bit
+curses.flash()
+
+# set the cursor to be invisible
+curses.curs_set(0)
+
+# writing to the terminal, (y,x,text)
+scr.addstr(0,0,"Test")
+scr.addstr(1,0,"Press any key to continue")
+
+# update terminal screen
+scr.refresh()
+
+# get a character of input 
+inpt = scr.getstr()
+scr.addstr(2,0,inpt)
+scr.refresh()
+
+time.sleep(2)
+
+curses.nocbreak()
+curses.echo()
+curses.curs_set(1)
+curses.endwin()
